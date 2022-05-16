2f47708a54a13f40fd8487591b9e13f7f4ca4d82
Matthew <mfbg@Matthews-Air.fritz.box>
2022-05-16 20:12:03 +0200

    Fixed bug

--- main.cpp
+++ main.cpp
@@ -34,20 +34,23 @@ void endOfLineProcedure(ifstream& str_, vector<float>& vals_, vector<char>& ops_
 void pushAndClear(vector<float>& vals_, string& num_);
 void lineClear(ifstream& str_, vector<float>& vals_, vector<char>& ops_, string& num_, int& bcount_);
 
-int main(int argc, char** argv) {
-    if(argc > 1) {
-        // Stream wird geöffnet
+int main(int argc, char** argv)
+{
+    if(argc > 1)
+	{
         ifstream str(argv[1]);
 
-        // Es wird getestet ob Stream nicht geöffnet wurde
-        if(!str.is_open()) cout << "Fehler: Datei konnte nicht gefunden oder geoeffnet werden." << endl;
-        // Jetzt können die Gleichungen in der eingegeben Datei evaluiert werden
-        else evaluate(str);
+        if(!str.is_open())
+			cout << "Fehler: Datei konnte nicht gefunden oder geoeffnet werden." << endl;
+        else
+			evaluate(str);
 
-        // Stream wird geschlossen
         str.close();
     }
-    else cout << "Fehler: Kein Dateiname wurde eingegeben." << endl;
+    else
+	{
+		cout << "Fehler: Kein Dateiname wurde eingegeben." << endl;		
+	}
 
     return 0;
 }
@@ -55,7 +58,8 @@ int main(int argc, char** argv) {
 // Shunting-Yard Algorithmus zur Arithmetik-Evaluierung
 // Werte und Operationen werden nach und nach in Vektoren gemacht und
 // in der richtigen Reihenfolge, von Klammern diktiert, evaluiert
-void evaluate(ifstream& str_) {
+void evaluate(ifstream& str_)
+{
     // Zwei Vektoren werden deklariert für die Werte und die Operatoren
     vector<float> vals;
     vector<char> ops;
@@ -63,61 +67,76 @@ void evaluate(ifstream& str_) {
     // num stellt die einzulesende Zahl dar
     // num ist ein String weil ein Punkt und/oder beliebig viel Nachkommastellen können kommen
     // Bevor num in den Wert-Vektor kommt, wird es in ein Float umgewandelt
-    string num;
+    string num = "";
 
     // Die Anzahl von Klammern wird verfolgt
-    int bcount;
+    int bcount = 0;
 
     // Ein Charakter nach dem anderen wird eingelesen
     char c;
-    while(str_.get(c)) {
-            // Leerzeichen werden einfach übersprungen
-        if(c == ' ') continue;
-        // Wenn ein Kommentar kommt, wird die Linie übersprungen
-        else if(c == '#') lineClear(str_, vals, ops, num, bcount);
+
+    while(str_.get(c))
+	{
+        if(c == ' ')
+			continue;
+        else if(c == '#')
+        	// Wenn ein Kommentar kommt, wird die Linie übersprungen
+			lineClear(str_, vals, ops, num, bcount);
         // c ist entweder eine öffnende Klammer oder ein Operator
         // Da Terme vollständig geklammert sind, ist die Reihenfolge von Operatoren irrelevant
-        else if(c == '(' || c == '+' || c == '-' || c == '*' || c == '/' || c == '~') {
-            // Wenn c ein Operator ist, füge es zum Operator-Vektor hinzu
-            if(c != '(') ops.insert(ops.begin(), c);
-
-            // Eine öffnende Klammer - bcount wird erhöht
-            else bcount++;
+        else if(c == '(' || c == '+' || c == '-' || c == '*' || c == '/' || c == '~')
+		{
+            if(c != '(')
+				ops.insert(ops.begin(), c);
+            else
+ 				// Eine öffnende Klammer - bcount wird erhöht
+				bcount++;
 
             // Da die Zahl zu Ende ist, kann num in den Wert-Vektor getan werden
-            if(!num.empty()) pushAndClear(vals, num);
+            if(!num.empty())
+				pushAndClear(vals, num);
         }
         // Die erste Operation in dem Operation-Vektor muss mit den
         // ersten Werten durchgeführt werden
-        else if(c == ')') {
+        else if(c == ')')
+		{
             // Eine schließende Klammer - 1 wird von bcount abgezogen
             bcount--;
 
             // num kommt erst zum Wert-Vektor wenn es nicht leer ist
-            if(!num.empty()) {
+            if(!num.empty())
+			{
                 pushAndClear(vals, num);
-                // Dies kümmert sich um die erste schließende Klammer, falls es eine Reihe davon gibt
-                if(!ops.empty()) performOperation(vals, ops);
+                if(!ops.empty())
+                	// Dies kümmert sich um die erste schließende Klammer, falls es eine Reihe davon gibt
+					performOperation(vals, ops);
             }
             // Eine weitere Operation wird ausgeführt wenn beide Vektoren nicht leer sind
             // ALLERDINGS wenn num leer ist und es noch Operationen im Vektor gibt,
             // obwohl die Klammerzahl nicht gleich 0 ist, sollte die Operation nicht ausgeführt werden
             // Dies erlaubt (unnötige) Doppelklammern, wie in Term 5 (meiner Testdatei)
-            else if(!vals.empty() && !ops.empty() && bcount == 0) performOperation(vals, ops);
+            else if(!vals.empty() && !ops.empty() && bcount == 0)
+				performOperation(vals, ops);
         }
         // Zeilenende - Operation wird eventuell ausgeführt
-        else if(c == '\n') endOfLineProcedure(str_, vals, ops, num, bcount);
-        else { // c sollte Teil einer Zahl sein
+        else if(c == '\n')
+			endOfLineProcedure(str_, vals, ops, num, bcount);
+        else  // c sollte Teil einer Zahl sein
+		{ 
             // Falls c ein Punkt ist
-            if(c == 46) num += ".";
+            if(c == 46)
+				num += ".";
             // Sonst wird der Ziffer zu num hinzugefügt
-            else {
+            else
+			{
                 // Falls etwas nicht Numerisches in der Datei ist
-                if(!isdigit(c)) {
+                if(!isdigit(c))
+				{
                     cout << "Fehler: keine nicht numerischen Charaktere erlaubt." << endl;
                     lineClear(str_, vals, ops, num, bcount);
                 }
-                else num += to_string(c-48); // -48 wegen UTF-8 Kodierung
+                else
+					num += to_string(c-48); // -48 wegen UTF-8 Kodierung
             }
         }
     }
@@ -127,19 +146,23 @@ void evaluate(ifstream& str_) {
 
 void endOfLineProcedure(ifstream& str_, vector<float>& vals_, vector<char>& ops_, string& num_, int& bcount_) {
     // Wenn die Klammeranzahl nicht aufgeht, ist das schlecht
-    if(bcount_ != 0) {
+    if(bcount_ != 0)
+	{
         cout << "Syntaxfehler: fehlende Klammern." << endl;
         lineClear(str_, vals_, ops_, num_, bcount_);
     }
     // Wenn am Ende der Zeile eine Zahl noch nicht in den Wert-Vektor gekommen ist, wird das jetzt gemacht
-    if(!num_.empty()) pushAndClear(vals_, num_);
+    if(!num_.empty())
+		pushAndClear(vals_, num_);
 
     // Da num nur in den Wert-Vektor getan wird wenn ein Operator oder eine schließende Klammer kommt,
     // kann es sein, dass noch eine Operation ausgeführt werden muss
-    if(!ops_.empty()) performOperation(vals_, ops_);
+    if(!ops_.empty())
+		performOperation(vals_, ops_);
 
     // Ergebnis wird anzeigt und der oberste Element des Wert-Vektors entfernt
-    if(!vals_.empty()) {
+    if(!vals_.empty())
+	{
         cout << vals_.front() << endl;
         vals_.erase(vals_.begin());
     }
